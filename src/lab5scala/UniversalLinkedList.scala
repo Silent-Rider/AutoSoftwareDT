cbe921a4e52c92ed83f62f943022550f17f5bced
silent_rider <klimenko.k@v2grp.ru>
2026-03-18 23:13:47 +0700

    Допилил класс на Скала

--- src/lab5scala/UniversalLinkedList.scala
+++ src/lab5scala/UniversalLinkedList.scala
@@ -1,5 +1,7 @@
 package lab5scala;
 
+import java.util.Comparator
+
 class UniversalLinkedList[T] (val userType: UserType[T]) {
 
   private class Node (var value: T) {
@@ -65,4 +67,144 @@ class UniversalLinkedList[T] (val userType: UserType[T]) {
     current
   }
 
+  def add(index: Int, value: T): Unit = {
+    if (index < 0 || index > size) {
+      throw new IndexOutOfBoundsException(s"Index: $index, Size: $size")
+    }
+    if (index == size) {
+      addLast(value)
+      return
+    }
+    if (index == 0) {
+      addFirst(value)
+      return
+    }
+
+    val nextNode = getNodeByIndex(index)
+    val prevNode = nextNode.previous
+    val newNode = new Node(value)
+    
+    prevNode.next = newNode
+    newNode.previous = prevNode
+    newNode.next = nextNode
+    nextNode.previous = newNode
+
+    size += 1
+  }
+
+  def set(index: Int, value: T): T = {
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException(s"Index: $index, Size: $size")
+    }
+    val nodeToSet = getNodeByIndex(index)
+    val oldValue = nodeToSet.value
+
+    nodeToSet.value = value
+    oldValue
+  }
+
+  def remove(index: Int): T = {
+    if (index < 0 || index >= size) {
+      throw new IndexOutOfBoundsException(s"Index: $index, Size: $size")
+    }
+
+    val nodeToRemove = getNodeByIndex(index)
+    val removedValue = nodeToRemove.value
+    
+    if (nodeToRemove.eq(first) && nodeToRemove.eq(last)) {
+      first = null
+      last = null
+    } else if (nodeToRemove.eq(first)) {
+      first = nodeToRemove.next
+      first.previous = null
+    } else if (nodeToRemove.eq(last)) {
+      last = nodeToRemove.previous
+      last.next = null
+    } else {
+      val prev = nodeToRemove.previous
+      val next = nodeToRemove.next
+      prev.next = next
+      next.previous = prev
+    }
+
+    size -= 1
+    removedValue
+  }
+
+  def getSize: Int = size
+
+  def sort(): Unit = {
+    if (size < 2) return
+
+    first = mergeSort(first)
+    last = first
+    
+    while (last != null && last.next != null) {
+      last = last.next
+    }
+  }
+
+  private def mergeSort(head: Node): Node = {
+    if (head == null || head.next == null) {
+      return head
+    }
+
+    val middle = getMiddle(head)
+    val nextOfMiddle = middle.next
+    middle.next = null
+
+    val left = mergeSort(head)
+    val right = mergeSort(nextOfMiddle)
+
+    sortAndMergeLists(left, right)
+  }
+
+  private def getMiddle(head: Node): Node = {
+    var slow: Node = head
+    var fast: Node = head
+    
+    while (fast.next != null && fast.next.next != null) {
+      slow = slow.next
+      fast = fast.next.next
+    }
+    slow
+  }
+
+  private def sortAndMergeLists(a: Node, b: Node): Node = {
+    if (a == null) return b
+    if (b == null) return a
+
+    var result: Node = null
+    val comparator: Comparator[T] = userType.getTypeComparator
+
+    val diff = comparator.compare(a.value, b.value)
+
+    if (diff <= 0) {
+      result = a
+      result.next = sortAndMergeLists(a.next, b)
+    } else {
+      result = b
+      result.next = sortAndMergeLists(a, b.next)
+    }
+
+    if (result.next != null) {
+      result.next.previous = result
+    }
+
+    result.previous = null
+    result
+  }
+
+  def forEach(action: T => Unit): Unit = {
+    var current = first
+    while (current != null) {
+      action(current.value)
+      current = current.next
+    }
+  }
+
+  def printList(): Unit = {
+    forEach { value => print(s"$value ") }
+    println()
+  }
 }
